fe30ef80424e68b8e023652e79493d3838277c46
WearySebas <sebichito@gmail.com>
2018-02-18 21:19:29 -0600

    MJ says hi

--- Project_MJ.py
+++ Project_MJ.py
@@ -1,50 +1,51 @@
 import TTS_Google
+import random
 #!/usr/bin/env python3
 # Requires PyAudio and PySpeech.
 
 import speech_recognition as sr
  
+hellos = ["hey", "hi", "hello", "how is it going"]
+def say_hello(hellos):
+    return random.choice(hellos)
+
 # Record Audio
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
+    mjAudio = r.recognize_google(audio)
  
 # Speech recognition using Google Speech Recognition
 try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print ("You said: " + r.recognize_google(audio))
+    #here goes everything MJ will do if she understand you
+    print ("You said: " + mjAudio)
+
+    #Profile set
+    #Process
+    #Math/Google Search/Reading of a file or web page
+    #Do It
+    #Get answer of what was asked for and what was asked for
+    #Interpretate Answer
+
+    #Return Spoken Answer
+    if mjAudio in hellos:
+        text = say_hello(hellos)
+    elif mjAudio == "hello there":
+        text = "general kenobi"
+    else:
+        text = "I am not self conscious, yet"
+
+    TTS_Google.speak(text,'en')
+    
 except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
+    TTS_Google.speak('I could not understand what you said','en')
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 #Profile set
-#class God (object):
-
-#    species = "Devine"
-
-#    def __init__ (self, user_Name, user):
-       
- #       self.user_Name = user_Name
-  #      self.user = user
-
-#WS = God ("WS","Sebastián")
-    
-#def setuser (WS):
- #       if WS.__class__ == God:
-  #          print (WS.user + " has full access.")
-
-#class CPU (object):
-
-
-
 #Process
 #Math/Google Search/Reading of a file or web page
 #Do It
 #Get answer of what was asked for and what was asked for
 #Interpretate Answer
 #Return Spoken Answer
-text = r.recognize_google(audio)
-TTS_Google.speak(text,'en')
