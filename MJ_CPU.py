99db60e6ad8a3bd5b36836685da5bb640e9d7f63
WearySebas <sebichito@gmail.com>
2018-04-11 12:46:07 -0500

    MJ math 1.0

--- MJ_CPU.py
+++ MJ_CPU.py
@@ -1,8 +1,10 @@
 import random
 import Google_Search
+import MJ_Math
+
 search = ["search","google","look"]
 hello = ["hello", "hey", "hi", "hello there"]
-
+operate = ["what's", "how much is", "solve"]
 
 def say_hello(mjAudio):
     hi = ("hello", "hey", "hi", "hello there")
@@ -16,6 +18,9 @@ def search_for (searchfor):
     Google_Search.google_search(searchfor)
     return "This is what you searched for"
 
+def solve (num1, num2, operator):
+    return str (MJ_Math.select_Math(num1, num2, operator))
+
 def select_command (mjAudio0,mjAudio1):
     if mjAudio0 in search:
         return search_for(mjAudio1)
@@ -23,4 +28,3 @@ def select_command (mjAudio0,mjAudio1):
         return say_hello(mjAudio0)
     else:
         return "I do not know how to do that yet"
-
--- Project_MJ.py
+++ Project_MJ.py
@@ -4,21 +4,25 @@ import Google_Search
 import STT
 import MJ_CPU
 
-#!/usr/bin/env python3
-# Requires PyAudio and PySpeech.
+#MJ will do any number of things until you say thank you (be polite)
+mjAudio = ""
+terminator = ["thank you MJ", "thanks MJ","that's it", "that's it MJ", "no", "no thaks", "no thank you"]
+while mjAudio not in terminator:
+    #recognize audio
+    mjAudio = STT.talk()
+    if mjAudio not in terminator: #Avoids another repetition when "thank you MJ is said"
+        #Separate by words
+        mj = mjAudio.split() 
+        #command to run
+        mjAudio0 = mj[0]
+        #Parameters for the command
+        mjAudio1 = mj[1:]
+        #Run command selector
+        text = MJ_CPU.select_command(mjAudio0,mjAudio1)
 
-#Recognize voice
-mjAudio = STT.talk()
-#Separate by words
-mj = mjAudio.split() 
-#command to run
-mjAudio0 = mj[0]
-#Parameters for the command
-mjAudio1 = mj[1:]
-
-#Run command selector
-text = MJ_CPU.select_command(mjAudio0,mjAudio1)
-
-# Speech recognition using Google Speech Recognition
-TTS_Google.speak(text,'en')
-    
+        # Speech recognition using Google Speech Recognition
+        TTS_Google.speak(text,'en')
+        #Ask for another command
+        TTS_Google.speak('do you need me to do something else?','en')
+else:
+    TTS_Google.speak("You are welcome",'en')    
